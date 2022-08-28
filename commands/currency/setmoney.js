0edae091c7026eb22519b90394d233765e49e9c5
Jesse <asdjkl.key@gmail.com>
2022-08-28 20:13:24 -0300

    currency

--- commands/currency/setmoney.js
+++ commands/currency/setmoney.js
@@ -15,7 +15,7 @@ module.exports = {
         let user = message.mentions.users.first();
         if(!user) user = message.author;
         const quantity = args[0];
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('No tienes permiso para usar este comando. Permiso necesario: `ADMINISTRADOR`')
+        if(!message.member.permissions.has('ADMINISTRATOR')) return message.reply('No tienes permiso para usar este comando. Permiso necesario: `ADMINISTRADOR`')
         if(user.id == client.user.id || user.id == message.guild.me.id) return message.reply('No puedes establecer mi economia')
         if(user.bot) return message.reply('El usuario es un bot y no se puede establecer una economia');
         if(!quantity) return message.reply('Especifica la cantidad a ser colocada');
