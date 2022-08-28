559733979f3ad3a3b30ed5ada47b867e1ed9ab60
Jesse <asdjkl.key@gmail.com>
2022-08-27 23:52:51 -0300

    currency

--- commands/currency/deposit.js
+++ commands/currency/deposit.js
@@ -3,6 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 const { Database } = require('jesscode-lib');
 const db = new Database('currency');
 let coin = '<:Bahrs:1013192853687648306>';
+const { setCooldown, hasCooldown, replyCooldown } = require('../../utils/tools.js');
 
 module.exports = {
     name: 'deposit',
@@ -18,23 +19,33 @@ module.exports = {
                 wallet: 0
             })
         }
+        if(hasCooldown(`${user.id}:deposit`)){
+            return replyCooldown(message, `${user.id}:deposit`)
+        }
         const ballance = await db.get(`${user.id}`);
-        let quantity = args[0];
+        let quantity = args[0] === 'all' ? 'all' : parseInt(args[0]);
         if(!quantity) return message.reply('Debes especificar una cantidad');
         if(quantity !== 'all' && isNaN(quantity)) return message.reply('Ingresa una cantidad válida');
-        if(quantity <= 0) return message.reply('No tienes suficientes **Bahrs** '+coin+' para depositar');
+        if(quantity <= 0) return message.reply('La cantidad de '+coin+' **Bahrs** no puede ser igual o menor a 0');
+        if(quantity === 'all' && ballance.wallet <= 0) return message.reply('No tienes suficientes '+coin+' **Bahrs** para depositar')
         let embed = new EmbedBuilder()
         .setAuthor({name: user.tag, iconURL: user.displayAvatarURL()})
         .setColor('#00fc00')
-        .setDescription(`${coin} ${(quantity === 'all' ? ballance.wallet + ballance.bank : quantity)} han sido depositados en el banco.`)
+        .setDescription(`${coin} ${(quantity === 'all' ? ballance.wallet + ballance.bank : quantity)} **Bahrs** han sido depositados en el banco.`)
         .setTimestamp()
-        .setFooter({text: user.tag, iconURL: user.displayAvatarURL});
+        .setFooter({text: user.tag, iconURL: user.displayAvatarURL()});
         message.reply({embeds: [embed]})
-        ballance.bank += ballance.wallet;
-        ballance.wallet = 0;
-        return db.set(`${user.id}`, {
-            bank: ballance.bank,
-            wallet: ballance.wallet
-        })
+        setCooldown(10000)
+        if(quantity === 'all'){
+            ballance.bank += ballance.wallet;
+            ballance.wallet = 0;
+            return db.set(`${user.id}`, {
+                bank: ballance.bank,
+                wallet: ballance.wallet
+            })
+        }
+        ballance.bank += quantity;
+        ballance.wallet -= quantity;
+        return db.set(`${user.id}`, ballance)
     }
 }
--- utils/tools.js
+++ utils/tools.js
@@ -1,8 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
 const { botProperties } = require('./database');
-//const { Database } = require('jesscode-lib');
-//const db = new Database('<name>');
-
+const { Database } = require('jesscode-lib');
+const cool = new Database('cooldown');
 
 class _ET{
     constructor(data){
@@ -30,4 +29,81 @@ class _ET{
     }
 }
 
-module.exports = { _ET }
+const setCooldown = (cooldown = 0, topic = '') => {
+    if(!cooldown) return new Error('No cooldown provided');
+    if(isNaN(cooldown)) return new Error('The cooldown provided isNaN');
+    if(cooldown <= 0) return new Error("the cooldown provided must not be less than or equal to 0");
+    if(!topic) return new Error('No topic provided');
+    if(typeof topic !== 'string') return new Error('The topic provided must be a string');
+    cool.set(topic, Date.now() + cooldown);
+}
+const hasCooldown = async(topic = '') => {
+    if(!topic) return new Error('No topic provided');
+    if(typeof topic !== 'string') return new Error('The topic must be a string');
+    if(!cool.has(topic)) return false;
+    if(await cool.get(topic) <= Date.now()){
+        cool.remove(topic);
+        return false;
+    }
+    if(await cool.get(topic) > Date.now()) return true;
+}
+const convertTime = async(time) => {
+    let d = [0,0]
+    let h = [0,0];
+    let min = [0,0];
+    let s = [0,0];
+
+    let timeInHour;
+    let timeInMin;
+    let timeInS;
+    if(!time) return new Error('missing time');
+    if(isNaN(time)) return new Error('the time provided must be a number');
+    if(time > 3600000){
+        timeInHour = Math.floor(time/3600000);
+        if(timeInHour > 9){
+            timeInHour = timeInHour.toString();
+            h[0] = timeInHour[0];
+            h[1] = timeInHour[1];
+        } else {
+            h[1] = timeInHour.toString();
+        }
+    }
+    if(time > 60000){
+        timeInMin = Math.floor(time/60000);
+        if(timeInMin > 9){
+            timeInMin = timeInMin.toString();
+            min[0] = timeInMin[0];
+            min[1] = timeInMin[1];
+        } else {
+            min[1] = timeInMin.toString();
+        }
+    }
+    if(time > 1000){
+        timeInS = Math.floor(time/1000);
+        if(timeInS > 9){
+            timeInS = timeInS.toString();
+            s[0] = timeInS[0];
+            s[1] = timeInS[1];
+        } else {
+            s[1] = timeInS.toString();
+        }
+    }
+    let payload = `${h[0] === 0 && h[1] === 0 ? '': h[0]+h[1]+'h, '}${min[0] === 0 && min[1] === 0 ? '': min[0]+min[1]+'min y'}${s[0] === 0 && s[1] === 0 ? '': s[0]+s[1]+'s'}`;
+    return payload;
+}
+
+const replyCooldown = async(message, topic) => {
+    if(!message || !topic) return new Error('missing data');
+    let cooldown = await cool.get(topic);
+    if(hasCooldown(topic) === false) return;
+    let embed = new EmbedBuilder()
+    .setAuthor({name: message.author.tag, iconURL: message.author.displayAvatarURL()})
+    .setColor('#fc0000')
+    .setDescription(`Debes esperar ${convertTime(cooldown - Date.now())} para usar este comando`)
+    .setTimestamp()
+    .setFooter({text: message.author.tag, iconURL: message.author.displayAvatarURL()});
+
+    return message.reply({embeds: [embed]});
+}
+
+module.exports = { _ET, setCooldown, replyCooldown, hasCooldown }
