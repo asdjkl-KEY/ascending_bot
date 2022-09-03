f909007c1c45a330d69831d7ab36b36ec979de4b
Jesse <asdjkl.key@gmail.com>
2022-09-03 10:13:08 -0300

    currency

--- commands/currency/inventory.js
+++ commands/currency/inventory.js
@@ -3,23 +3,10 @@ const { EmbedBuilder } = require('discord.js');
 const { Database, ToolKit } = require('jesscode-lib');
 const bags = new Database('bags');
 let emotes = require('../../helpers/emotes.js');
-const tk = new ToolKit();
+let dic = require('../../helpers/dictionary');
 // const { setCooldown, hasCooldown, replyCooldown } = require('../../utils/tools.js');
+const tk = new ToolKit();
 
-
-function paginate(data, numPerPage){
-    let pages = [];
-    if(!Array.isArray(data)) return new Error('The data provided has not an array');
-    let page = [];
-    for(let d of data){
-        if(page.length === numPerPage){
-            pages.push(page);
-            page = [];
-        }
-        page.push(d);
-    }
-    return pages;
-}
 module.exports = {
     name: 'inventory',
     alias: ['bag'],
@@ -30,21 +17,25 @@ module.exports = {
         const user = message.author;
         let bag = await bags.get(`${user.id}`);
         let itemKeys = Object.keys(bag);
-        let pages = paginate(itemKeys, 10);
+        let pages = tk.paginate(itemKeys, 10);
         let page = parseInt(args[0]) -1;
         if(isNaN(parseInt(args[0])) || !args[0] || parseInt(args[0])-1 <= 0) page = 0;
         let items = pages[page];
         let embed = new EmbedBuilder()
         .setAuthor({name: user.username, iconURL: user.displayAvatarURL()})
         .setColor('#00fc00')
-        .setFields(items.map(i => {
-            return {
-                name: i,
-                value: 'x'+bag[i].quantity
-            }
-        }))
         .setFooter({text: `Página ${page+1}/${pages.length}`})
         .setTimestamp();
+        if(pages.length > 0){
+            embed.setFields(items.map(i => {
+                return {
+                    name: emotes[bag[i].emote] + ' '+dic[i],
+                    value: 'x'+bag[i].quantity
+                }
+            }));
+        } else {
+            embed.setDescription(`**Tu inventario está vacío  >-<**`);
+        }
         message.reply({embeds: [embed]});
     }
 }
---
+++ helpers/dictionary.js
@@ -0,0 +1,7 @@
+let dic = {
+    cobblestone: "Roca",
+    iron: "Hierro",
+    gold: "Oro",
+    pickaxe: "Pico",
+    rocavital: "Roca Vital",
+}
