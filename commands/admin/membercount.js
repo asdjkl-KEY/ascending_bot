38a1ae8bd3a4a93fb6683ff7d8c4da9ea5958347
Jesse <asdjkl.key@gmail.com>
2022-12-19 22:25:49 -0300

    admin command

--- commands/admin/membercount.js
+++ commands/admin/membercount.js
@@ -21,7 +21,7 @@ module.exports = {
             db.set(message.guild.id, guild);
         }
         let guild = await db.get(message.guild.id);
-        console.log(message.guild.members.cache+ "\n\n"+ message.guild.members.fetch())
+        console.log(message.guild.members.cache.size+ "\n\n"+ await message.guild.members.fetch())
         let members = await message.guild.members.fetch();
         let total = members.size;
         let bots = members.filter(m => m.user.bot).size;
