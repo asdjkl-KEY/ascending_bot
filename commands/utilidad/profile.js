0e4dc0af49e9809807cdcbdd259151f023d8f7b7
Jesse <asdjkl.key@gmail.com>
2022-12-20 00:14:55 -0300

    util command

--- commands/utilidad/profile.js
+++ commands/utilidad/profile.js
@@ -11,6 +11,36 @@ module.exports = {
     usage: '<prefix> profile [usuario]',
     async execute(client, message, args, R){
         let user = message.mentions.users.first() || message.author;
+        let date = {
+            day: {
+                Mon: "Lunes",
+                Tue: "Martes",
+                Wed: "Miércoles",
+                Thu: "Jueves",
+                Fri: "Viernes",
+                Sat: "Sábado",
+                Sun: "Domingo"
+            },
+            month: {
+                Jan: "Enero",
+                Feb: "Febrero",
+                Mar: "Marzo",
+                Apr: "Abril",
+                May: "Mayo",
+                Jun: "Junio",
+                Jul: "Julio",
+                Aug: "Agosto",
+                Sep: "Septiembre",
+                Oct: "Octubre",
+                Nov: "Noviembre",
+                Dec: "Diciembre"
+            }
+        }
+        let cAt = user.createdAt+"".split(" ");
+        let createdAt = `${date.day[cAt[0]]}, ${cAt[2]} de ${date.month[cAt[1]]} de ${cAt[3]} a las ${cAt[4]}`;
+        let jAt = message.guild.members.cache.get(user.id).joinedAt+"".split(" ");
+        let joinedAt = `${date.day[jAt[0]]}, ${jAt[2]} de ${date.month[jAt[1]]} de ${jAt[3]} a las ${jAt[4]}`;
+
         let embed = new R.embed()
             .setColor('#ff0000')
             .setTitle(`Perfil de ${user.username}`)
@@ -20,9 +50,9 @@ module.exports = {
                 { name: 'Tag', value: user.tag, inline: true },
                 { name: 'ID', value: user.id, inline: true },
                 { name: 'Bot', value: user.bot ? "Verdadero" : "Falso", inline: true },
-                { name: 'Cuenta creada el', value: user.createdAt+"", inline: true },
-                { name: 'Roles', value: message.guild.members.cache.get(user.id).roles.cache.map(r => r).join(' | '), inline: true },
-                { name: 'Se unió a este servidor el', value: message.guild.members.cache.get(user.id).joinedAt+"", inline: true},
+                { name: 'Cuenta creada el', value: createdAt, inline: true },
+                { name: 'Roles', value: message.guild.members.cache.get(user.id).roles.cache.map(r => r).join(' '), inline: true },
+                { name: 'Se unió a este servidor el', value: joinedAt, inline: true},
                 { name: 'Nickname', value: message.guild.members.cache.get(user.id).nickname ? message.guild.members.cache.get(user.id).nickname : "Ninguno", inline: true }
 
             )
