aa02a7ca4b01829f0779cf055134e8a65eb76eb4
Jesse <asdjkl.key@gmail.com>
2022-12-20 12:20:21 -0300

    admin command

--- commands/admin/clearroles.js
+++ commands/admin/clearroles.js
@@ -12,14 +12,49 @@ module.exports = {
     async execute(client, message, args, R){
         let roles = message.guild.roles.cache;
         let count = 0;
-        roles.forEach(async role => {
-            role.delete()
-            .then(deleted => {
-                console.log(`Eliminado el rol ${deleted.name}`)
-                count++;
-            })
-            .catch(err => console.log(err));
-        });
-        message.channel.send({content: "**"+count+"**"+' Roles eliminados.'});
+        //confirmation with reaction
+        let embed = new R.embed()
+        .setTitle('¿Estás seguro?')
+        .setDescription('Si reacciona con ✅, todos los roles serán eliminados.')
+        .setColor('#fcfc00')
+        .setFooter({text: 'pedido por: '+message.author.tag, iconURL: message.author.displayAvatarURL({dynamic: true})})
+        .setTimestamp()
+        let msg = await message.channel.send({embeds: [embed]})
+        await msg.react('✅');
+        await msg.react('❌');
+        let filter = (reaction, user) => {
+            return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+        }
+        let collector = msg.createReactionCollector({filter, time: 60000});
+        collector.on('collect', async (reaction, user) => {
+            if(reaction.emoji.name === '✅' && user.id === message.author.id){
+                collector.stop();
+                try{
+                    msg.delete();
+                } catch (err) {
+                    console.log(err);
+                }
+                await clearRoles();
+            }else if(reaction.emoji.name === '❌' && user.id === message.author.id){
+                collector.stop();
+                try {
+                    msg.delete();
+                } catch (err) {
+                    console.log(err);
+                }
+                message.channel.send({content: 'Comando cancelado.'});
+            }
+        })
+        async function clearRoles(){
+            roles.forEach(async role => {
+                role.delete()
+                .then(deleted => {
+                    console.log(`Eliminado el rol ${deleted.name}`)
+                    count++;
+                })
+                .catch(err => console.log(err));
+            });
+            message.channel.send({content: "**"+count+"**"+' Roles eliminados.'});
+        }
     }
 }
