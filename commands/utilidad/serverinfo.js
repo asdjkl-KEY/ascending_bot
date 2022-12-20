22d8949d691495bf2f492744a206401d7dae9e07
Jesse <asdjkl.key@gmail.com>
2022-12-20 08:48:09 -0300

    util command

--- commands/utilidad/serverinfo.js
+++ commands/utilidad/serverinfo.js
@@ -26,7 +26,7 @@ module.exports = {
             { name: 'Roles', value: server.roles.cache.size+"", inline: true },
             { name: 'Canales', value: server.channels.cache.size+"", inline: true },
             { name: 'Creado el', value: server.createdAt.toLocaleString(), inline: true },
-            { name: 'Región', value: server.region+"", inline: true },
+            { name: 'Baneos', value: server.bans.cache.size+"", inline: true },
             { name: 'Emojis', value: server.emojis.cache.size+"", inline: true },
             { name: 'Nitro Boosts', value: server.premiumSubscriptionCount+"", inline: true },
             { name: 'Nitro Boosts Nivel', value: server.premiumTier+"", inline: true },
