3e6bfcfbf5c539c89b9cb8fb2d98214ecd9bf280
Jesse <asdjkl.key@gmail.com>
2022-12-23 12:11:13 -0300

    new changes

--- commands/utils/top.js
+++ commands/utils/top.js
@@ -16,6 +16,37 @@ module.exports = {
         if(!type) type = 'xp';
         if(!types.includes(type)) return message.reply('El tipo de top no es válido.');
         let db = R.Databases.ranks;
+        if(type === 'money'){
+            let guild = await db.get(message.guild.id);
+            if(!guild) return message.reply('No hay datos para mostrar.');
+            if(Object.keys(guild). length === 0) return message.reply('No hay datos para mostrar.');
+            let datas = [];
+            let usersId = Object.keys(guild);
+            for (let i in usersId){
+                let info = guild[usersId[i]];
+                if(!info.ballance) continue;
+                let user = await client.users.fetch(usersId[i]);
+                info.user = user;
+                datas.push(info);
+            }
+            if(datas.length === 0) return message.reply('No hay datos para mostrar.');
+            datas.sort((a, b) => (b.ballance.wallet + b.ballance.bank) - (a.ballance.wallet + a.ballance.bank));
+            if(datas.length > 10){
+                let pages = Math.ceil(datas.length / 10);
+                if(page > pages) return message.reply('La página no existe.');
+                let data = datas.slice((page - 1) * 10, page * 10);
+                let embed = new R.embed()
+                    .setColor('#00fc00')
+                    .setTitle(`Top de ${type} del servidor ${message.guild.name}`)
+                    .setFooter({ text: `página ${page}/${datas.length}`, iconURL: message.author.displayAvatarURL() });
+                let description = '';
+                for(let i = 0; i < data.length; i++){
+                    description += `${i + 1} ${i+1 === 1 ? '🥇' : i+1 === 2 ? '🥈' : i+1 === 3 ? '🥉' : ''} ${data[i].user.tag} \`${type.toUpperCase()}: ${data[i].ballance.wallet + data[i].ballance.bank}\`\n`;
+                }
+                embed.setDescription(description);
+                return message.reply({ embeds: [embed] });
+            }
+        }
         db.get(message.guild.id).then(async guild => {
             if(!guild) return message.reply('No hay datos para mostrar.');
             if(Object.keys(guild). length === 0) return message.reply('No hay datos para mostrar.');
@@ -23,8 +54,7 @@ module.exports = {
             let usersId = Object.keys(guild);
             for(let i = 0; i < usersId.length; i++){
                 let user = await client.users.fetch(usersId[i]);
-                if(!guild[usersId[i]][type] && type != 'money') continue;
-                if(type == 'money' && !guild[usersId[i]].ballance) continue;
+                if(!guild[usersId[i]][type]) continue;
                 let info = guild[usersId[i]];
                 info.user = user;
                 datas.push(info);
