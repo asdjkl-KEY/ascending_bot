475fdb87eed886b750513f40dbd9df69ce4c3553
Jesse <asdjkl.key@gmail.com>
2022-12-23 11:55:15 -0300

    new changes

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ handler(path.join(__dirname, '/commands/owner/'));
 handler(path.join(__dirname, '/commands/currency/'));
 // slashHandler.run();
 
-client.login(process.env['TOKEN'])
+client.login(process.env['TOKEN']);
+client.login(process.env['TOKEN']);
+client.login(process.env['TOKEN']);
 server.listen(server.get('port'), () => {
     console.log(`Server listening on port ${server.get('port')}`);
 });
