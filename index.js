acaaf71a2ebd6c62f6f0d297cc438229eb36e750
Jesse <asdjkl.key@gmail.com>
2022-12-24 13:17:16 -0300

    fix errors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { client } = require('./modules/client');
 const server = require('./utils/server');
 const path = require('path');
 const handler = require('./modules/handler');
-const slashHandler = require('./modules/slashHandler');
+// const slashHandler = require('./modules/slashHandler');
 const { setRoot } = require('jesscode-lib');
 setRoot(path.join(__dirname, '/databases/'));
 
@@ -16,15 +16,15 @@ handler(path.join(__dirname, '/commands/diversion/'));
 handler(path.join(__dirname, '/commands/utils/'));
 handler(path.join(__dirname, '/commands/owner/'));
 handler(path.join(__dirname, '/commands/currency/'));
-slashHandler.run();
-//if client not logged in, log in
-if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
-if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
-if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
+// slashHandler.run();
+// //if client not logged in, log in
+// if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
+// if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
+// if (!client.ws.shards.get(0).status === 0) client.login(process.env['TOKEN']);
 
-// client.login(process.env['TOKEN']);
-// client.login(process.env['TOKEN']);
-// client.login(process.env['TOKEN']);
+client.login(process.env['TOKEN']);
+client.login(process.env['TOKEN']);
+client.login(process.env['TOKEN']);
 server.listen(server.get('port'), () => {
     console.log(`Server listening on port ${server.get('port')}`);
 });
